92b706dcd77236d7a0e2aa14ad3e7452aa7a7fe1
Schifferking <schifferking@hotmail.com>
2022-04-17 12:57:02 -0500

    Change color of a <div> when hovering
    With the mouseover eventListener, it can execute code when a cursor
    passes over an element, in this case a div, and then change the color
    to simulate the painting of a square.

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function createDivs() {
     
   for(let i = 1; i <= 256; i++) {    
     const newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'square');        
+    newDiv.setAttribute('class', 'square'); 
+    newDiv.addEventListener("mouseover", function(event) {
+        newDiv.style.backgroundColor = "black";
+    });
     container.appendChild(newDiv);
   }
 }
